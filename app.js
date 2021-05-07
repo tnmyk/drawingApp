54478e3867ade220d0940555568772401b44da13
tanmayk5 <73903781+tanmayk5@users.noreply.github.com>
2021-05-07 14:54:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+window.addEventListener("load", () => {
+  var linewidth = 7;
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d", { alpha: false });
+  canvas.width = window.innerWidth - 4;
+  canvas.height = window.innerHeight - 65.6;
+  ctx.strokeStyle = "black";
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(-1, -58);
+  var painting = false;
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
+  function stopPosition() {
+    painting = false;
+    ctx.beginPath();
+  }
+  function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = linewidth;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+  }
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", stopPosition);
+  canvas.addEventListener("mousemove", draw);
+
+  window.addEventListener("resize", () => {
+    reset();
+    ctx.fillStyle = "white";
+  });
+
+  var color = document.querySelector("#color");
+  color.addEventListener("change", () => {
+    ctx.strokeStyle = color.value;
+  });
+
+  document.querySelector("#bcolor").addEventListener("change", () => {
+    ctx.fillStyle = document.querySelector("#bcolor").value;
+    reset();
+  });
+  document.querySelector("#delete").addEventListener("click", () => {
+    reset();
+  });
+  function reset() {
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+    ctx.fillRect(0, 0, canvas.width, canvas.height + 66);
+  }
+
+  document.querySelector("#save").addEventListener("click", () => {
+    const dataURI = canvas.toDataURL();
+    const a = document.querySelector("#download");
+    a.href = dataURI;
+    a.download = "canvas-image.png";
+  });
+
+  document.querySelector("#size").addEventListener("change", () => {
+    linewidth = parseInt(document.querySelector("#size").value);
+    console.log(ctx.lineWidth);
+  });
+});
