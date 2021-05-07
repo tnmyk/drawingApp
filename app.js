ff498278be1dee5fd5bf927371a2334ad0aee6b4
tanmayk5 <73903781+tanmayk5@users.noreply.github.com>
2021-05-07 19:57:24 +0530

    pen color :)

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", () => {
   var linewidth = 7;
+  var color="black"
+  var backgroundcolor ="white"
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d", { alpha: false });
   canvas.width = window.innerWidth - 4;
   canvas.height = window.innerHeight - 65.6;
-  ctx.strokeStyle = "black";
-  ctx.fillStyle = "white";
+ 
+  ctx.fillStyle = backgroundcolor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.translate(-1, -58);
   var painting = false;
@@ -18,6 +20,7 @@ window.addEventListener("load", () => {
     ctx.beginPath();
   }
   function draw(e) {
+     ctx.strokeStyle = color;
     if (!painting) return;
     ctx.lineWidth = linewidth;
     ctx.lineCap = "round";
@@ -34,13 +37,14 @@ window.addEventListener("load", () => {
   window.addEventListener("resize", () => {
     reset();
     ctx.fillStyle = "white";
+    backgroundcolor = "white"
   });
   canvas.style.cursor = "url(/img/pencil.svg)0 25,auto";
   var penchoosen =true;
   document.querySelector('#pen').addEventListener('click',()=>{
     if(penchoosen) return;  
     canvas.style.cursor = "url(/img/pencil.svg)0 20,auto";
-    ctx.strokeStyle = color.value;
+    ctx.strokeStyle = color;
       linewidth -= 10;
       penchoosen=true;
   });
@@ -48,16 +52,38 @@ window.addEventListener("load", () => {
     if(!penchoosen) return;
     canvas.style.cursor = "url(./img/eraser.svg)0 15,auto";
     linewidth+=10;
-    ctx.strokeStyle = document.querySelector("#bcolor").value;
+    color = document.querySelector("#bcolor").value;
     penchoosen=false;
   });
 
-
-  var color = document.querySelector("#color");
-  color.addEventListener("change", () => {
-    ctx.strokeStyle = color.value;
+  var colormenu=document.querySelector('#color-menu');
+  var setcolor = document.querySelector("#setcolor");
+  setcolor.addEventListener("click", () => {
+    colormenu.style.right = "0vh";
   });
-
+  colormenu.addEventListener("click",(e)=>{
+    var target = e.target;
+    switch (target.id) {
+      case "black":
+        color = "black";
+        break;
+      case "white":
+        color = "white";
+        break;
+      case "red":
+        color = "red";
+        break;
+      case "green":
+        color = "greenyellow";
+        break;
+      case "blue":
+        color = "blue";
+        break;
+      default:
+        break;
+    }
+    colormenu.style.right="-30vh";
+  })
   document.querySelector("#bcolor").addEventListener("change", () => {
     ctx.fillStyle = document.querySelector("#bcolor").value;
     reset();
